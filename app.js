9350ba09b599e04d04ee2552ae0881a61ca8af88
Kunli <dengkunli@foxmail.com>
2016-01-25 21:37:35 +0100

    improve color display scheme

--- app.js
+++ app.js
@@ -14,9 +14,41 @@ L.TopoJSON = L.GeoJSON.extend({
 
 
 (function() {
+    function HSVtoRGB(h, s, v) {
+        var r, g, b, i, f, p, q, t;
+        if (arguments.length === 1) {
+            s = h.s, v = h.v, h = h.h;
+        }
+        i = Math.floor(h * 6);
+        f = h * 6 - i;
+        p = v * (1 - s);
+        q = v * (1 - f * s);
+        t = v * (1 - (1 - f) * s);
+        switch (i % 6) {
+            case 0: r = v, g = t, b = p; break;
+            case 1: r = q, g = v, b = p; break;
+            case 2: r = p, g = v, b = t; break;
+            case 3: r = p, g = q, b = v; break;
+            case 4: r = t, g = p, b = v; break;
+            case 5: r = v, g = p, b = q; break;
+        }
+        return {
+            r: Math.round(r * 255),
+            g: Math.round(g * 255),
+            b: Math.round(b * 255)
+        };
+    }
+
+    function rgbToHex(r, g, b) {
+        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+
     function getColor(score) {
-        score = (score - 4) / 67 * 0xfffff;
-        return "#" + ("0" + Math.trunc(score)).slice(-6);
+        //score = (score - 4) / 67 * 0xfffff;
+        //return "#" + ("0" + Math.trunc(score)).slice(-6);
+        score = (1 - (score - 4) / 67) / 6;
+        var rgb = HSVtoRGB(score, 1, 1);
+        return rgbToHex(rgb.r, rgb.g, rgb.b);
     }
 
     function style(feature) {
