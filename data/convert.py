59e3a90d899fde260d97b2254532d4fee5b0f777
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-01-26 14:09:04 +0100

    Improve data

--- data/convert.py
+++ data/convert.py
@@ -1,19 +1,63 @@
+#!/usr/bin/env python3
+
 import json
 import csv
 
-def main():
-    with open('IMD_Preston_Complete.csv', newline='') as f:
-        data = list(csv.DictReader(f))
-
-    data = {row["LSOA11CD"]: row for row in data}
-
-    for lsoa11cd, lsoa in data.items():
-        for key, value in lsoa.items():
+def convert_numbers(d):
+    try:
+        for key, value in d.items():
             try:
-                lsoa[key] = float(value)
+                d[key] = float(value)
             except Exception:
                 pass
+            convert_numbers(d[key])
+    except Exception:
+        pass
 
+def filter_row(row):
+    return row
+
+def main():
+    with open('IMD_Preston_Complete.csv', newline='') as f:
+        raw_data = list(csv.DictReader(f))
+
+    data = {row["LSOA11CD"]: filter_row(row) for row in raw_data}
+    lsoas = data.keys()
+
+    for lsoa11cd, lsoa in data.items():
+        lsoa["PCD7s"] = [
+                row["PCD7"]
+                for row
+                in raw_data
+                if row["LSOA11CD"] == lsoa11cd
+        ]
+        lsoa["PCD8s"] = [
+                row["PCD8"]
+                for row
+                in raw_data
+                if row["LSOA11CD"] == lsoa11cd
+        ]
+
+    with open('Mean_Wellbeing_Scores_Preston_LSOA.csv', newline='') as f:
+        satisfaction = list(csv.DictReader(f))
+
+    for lsoa in satisfaction:
+        lsoa11cd = lsoa["REF"].split("/")[-1]
+        keys_to_keep = {
+            "How satisfied are you with your life",
+            "How happy did you feel yesterday",
+            "To what extent do you think the things you do are worthwhile"
+        }
+        if not lsoa11cd in data:
+            print("Unknown LSOA11CD: {}".format(lsoa11cd))
+            continue
+        data[lsoa11cd]["satisfaction"] = {
+            key: lsoa[key]
+            for key
+            in keys_to_keep
+        }
+
+    convert_numbers(data)
 
     with open('data.json', 'w') as f:
         f.write("window.data = ")
