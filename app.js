03a9c06fed6a67d66a4fe9c09b593bd0d857db15
Mikaël Fourrier <mikael.fourrier@etu.univ-nantes.fr>
2016-01-26 16:58:22 +0100

    Add stats functions

--- app.js
+++ app.js
@@ -12,6 +12,49 @@ L.TopoJSON = L.GeoJSON.extend({
     }
 });
 
+STATS = (function() {
+    "use strict";
+
+    function mean(indicator) {
+        var count;
+        return Object.keys(window.data)
+        .map(function (lsoa11cd) {
+            count += 1;
+            return window.data[lsoa11cd][indicator].raw;
+        }).reduce(function (a, b) {
+            return a + b;
+        }) / count;
+    }
+
+    function max(indicator) {
+        return Object.keys(window.data)
+        .map(function (lsoa11cd) {
+            return window.data[lsoa11cd][indicator].raw;
+        }).reduce(function (max, a) {
+            return a > max ? a : max;
+        });
+    }
+
+    function min(indicator) {
+        return Object.keys(window.data)
+        .map(function (lsoa11cd) {
+            return window.data[lsoa11cd][indicator].raw;
+        }).reduce(function (max, a) {
+            return a < max ? a : max;
+        });
+    }
+
+    function median(indicator) {
+        var values = Object.keys(window.data)
+        .map(function (lsoa11cd) {
+            return window.data[lsoa11cd][indicator].raw;
+        }).sort(function (a, b) {
+            return a - b;
+        });
+
+        return values[values.length() / 2];
+    }
+}());
 
 (function() {
     "use strict";
