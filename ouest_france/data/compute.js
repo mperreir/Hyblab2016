db328ab3b2e0bbe07519523d110c0bfc3d7c2b01
Valars <romaingar.garnier@gmail.com>
2016-01-27 23:51:30 +0100

    [NEW FILE] Calcul les pourcentages pour chaque année pour les carburants et les achats

---
+++ ouest_france/data/compute.js
@@ -0,0 +1,36 @@
+"use stricte";
+
+var fs = require('fs');
+
+fs.readFile('./data.json', 'utf8', function(err,data){
+		if(err){
+			throw err;
+		} 
+		else{
+			var donnees = JSON.parse(data); 
+			
+			var carburant = donnees.carburant;
+
+			compute(donnees.carburant);
+			compute(donnees.achat);
+			fs.writeFile('./data.json', JSON.stringify(donnees, null, 2), function(err){
+				if(err) throw err;
+				else console.log("Saved!");
+			})			
+		}	
+	}); 
+
+function compute(objet){
+	objet.years.forEach(function(year){
+		var total = 0;
+		for(element in objet.data){
+			total += objet.data[element][year].val;
+		}
+		console.log(year+" ==> "+total);
+
+		for(element in objet.data){
+			objet.data[element][year].pourcentage = objet.data[element][year].val/total*100;
+			console.log(element + " ==>" +objet.data[element][year].pourcentage);
+		}
+	});
+};
