cab9c646a89344cfc4c2385da585d450b1e6b6e2
Andrès Franco <andres.franco@etu.univ-nantes.fr>
2016-01-28 12:28:28 +0100

    First bar graph based on csv
    * Added style-loader in package.json
    * First scss file

--- the_city_talking/src/js/client.js
+++ the_city_talking/src/js/client.js
@@ -1,4 +1,5 @@
 const d3 = require('d3');
+import "../scss/main.scss";
 
 function parseDay(d){
 	return {
@@ -9,6 +10,37 @@ function parseDay(d){
 	}
 }
 
-d3.csv("data/average_sun.csv")
-	.row(parseDay)
-	.get((err,rows) => {console.log(rows)});
+let width = 960,
+	height = 500;
+
+let y = d3.scale.linear()
+	.range([height, 0]);
+
+let chart = d3.select(".chart")
+	.attr("width", width)
+	.attr("height", height);
+
+d3.csv("data/average_sun.csv",parseDay,(err,data) => {
+	y.domain([0, d3.max(data, function(d) { return d.NO; })]);
+
+	let barWidth = width / data.length;
+
+	let bar = chart.selectAll("g")
+		.data(data)
+		.enter()
+		.append("g")
+		.attr("transform",(d, i) =>  "translate(" + i * barWidth + ",0)" );
+
+	bar.append("rect")
+		.attr("y", (d) => y(d.NO) )
+		.attr("height", (d) =>  height - y(d.NO))
+		.attr("width", barWidth - 1);
+	/*
+	bar.append("text")
+		.attr("x", barWidth / 2)
+		.attr("y", function(d) { return y(d.value) + 3; })
+		.attr("dy", ".75em")
+		.text(function(d) { return d.value; });
+	});
+	*/
+});
