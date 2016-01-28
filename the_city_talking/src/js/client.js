67dc1dff22f8a167a4d35dc130131fc060043a1b
Andrès Franco <andres.franco@etu.univ-nantes.fr>
2016-01-28 12:01:45 +0100

    Added csv loading

--- the_city_talking/src/js/client.js
+++ the_city_talking/src/js/client.js
@@ -1,3 +1,14 @@
 const d3 = require('d3');
-let test = 2 ;
-alert(test);
+
+function parseDay(d){
+	return {
+		"Hour":d.Hour,
+		"NO":+d.NO,
+		"NO2":+d.NO2,
+		"PM25":+d.PM25,
+	}
+}
+
+d3.csv("data/average_sun.csv")
+	.row(parseDay)
+	.get((err,rows) => {console.log(rows)});
