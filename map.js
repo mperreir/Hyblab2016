7ece9c042cf12bd4749afa2fe5311bcf26a22ecb
Kunli <dengkunli@foxmail.com>
2016-01-29 13:18:59 +0100

    basic bar chart

--- map.js
+++ map.js
@@ -83,7 +83,6 @@ L.TopoJSON = L.GeoJSON.extend({
     (function() {
         var g = topo_msoa.objects.E07000123.geometries;
         for (var i in g) {
-            console.log(g[i]);
             MSOA[g[i].properties["MSOA11CD"]] = { "LSOAs" : [], "PCD7s" : [] };
         }
     }());
@@ -537,4 +536,78 @@ L.TopoJSON = L.GeoJSON.extend({
     };
     searchbar.addTo(map);
     searchbar.configEventListener();
+
+    var barchart = L.control({position : 'bottomright'});
+    barchart.onAdd = function(map) {
+        this._chartContainer = L.DomUtil.create('div','');
+        this._chartContainer.setAttribute('id', 'chartContainer');
+        //this._chartContainer.innerHTML = '<h1>adsf</h1>';
+        return this._chartContainer;
+    };
+    barchart.addTo(map);
+    // deciles
+    barchart.draw = function(deciles) {
+        var margin = {top: 20, right: 0, bottom: 20, left: 0},
+            width = 400 - margin.left - margin.right,
+            height = 300 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10, "%");
+
+        var svg = d3.select("#chartContainer").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain(['INCOME', 'HEALTH', 'EDUCATION', 'EMPLOYMENT', 'ENVIRONMENT', 'HOUSING', 'CRIME']);
+        y.domain(d3.range(10));
+
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis)
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Performance");
+
+        console.log(deciles);
+
+        svg.selectAll('.bar')
+            .data(deciles)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', function(d) { console.log(d);return x(d.indicator); })
+            .attr('width', x.rangeBand())
+            .attr('y', function(d) { return y(d.decile); })
+            .attr('height', function(d) { return height - y(d.decile); })
+    };
+    barchart.draw(
+        [   {'indicator':'INCOME',decile:5},
+            {'indicator':'HEALTH',decile:5},
+            {'indicator':'EDUCATION',decile:5},
+            {'indicator':'EMPLOYMENT',decile:5},
+            {'indicator':'ENVIRONMENT',decile:5},
+            {'indicator':'HOUSING',decile:5},
+            {'indicator':'CRIME',decile:5}]
+    );
 }());
