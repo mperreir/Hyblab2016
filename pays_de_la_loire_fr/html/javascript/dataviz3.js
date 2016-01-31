dfc379ac317dbe71dc27ef5f6cd3ac6f1d092666
e123645x <elouarn.laine@etu.univ-nantes.fr>
2016-01-31 02:13:47 +0100

    aiguille

--- pays_de_la_loire_fr/html/javascript/dataviz3.js
+++ pays_de_la_loire_fr/html/javascript/dataviz3.js
@@ -1,1 +1,26 @@
+'use strict';
 
+$(document).ready(function() {
+      
+	var s = Snap.select('#svg_i3');
+	var g = s.group();
+	/*var trotteuse = s.select("#trotteuse");*/
+	g.image("svg/aiguille-horloge.svg",0,0,200,200, function(loadedFragment) {
+                                                g.append(loadedFragment);
+                                                /*g.hover(hoverover, hoverout);*/
+                                        });
+
+	animTrotteuse(6);
+	function animTrotteuse(time){
+		var rot = time * 6;
+		g.stop().animate({ transform: 'r'+rot+', 160, 25'},
+		1000,mina.bounce,
+		function(){
+			var rot = time * 6;
+			g.attr({ transform: 'rotate('+rot+' 160 25)'});
+			animTrotteuse(++time);
+		});
+	}
+
+	
+});
