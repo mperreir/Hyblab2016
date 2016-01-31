b85e5a9922a6cd803da8ea7cf014438b4a5056e6
FreddyPoly <frederic.rollac@gmail.com>
2016-01-31 13:52:22 +0100

    Ajout des backgrounds dans resources, et uniformisation du fond des sections

--- pays_de_la_loire_fr/html/javascript/dataviz1.js
+++ pays_de_la_loire_fr/html/javascript/dataviz1.js
@@ -12,12 +12,21 @@ $(document).ready( function () {
 })
 
 function startQ () {
-	document.getElementById("sectionQ").style.display = "block";
+	//document.getElementById("sectionQ").style.display = "block";
+	$.fn.fullpage.moveSectionDown();
 }
 
 /* Fonction passant à la slide suivante (scroll horizontal) */
 function moveNextQ (button) {
 	$.fn.fullpage.moveSlideRight();
+	console.debug("Next : "+button);
+	var idQ = button[0]+button[1];
+	resetSlide(idQ);
+}
+
+/* Fonction passant à la section suivante (scroll vertical) */
+function moveNextS (button) {
+	$.fn.fullpage.moveSectionDown();
 	var idQ = button[0]+button[1];
 	resetSlide(idQ);
 }
@@ -26,10 +35,29 @@ function resetSlide (idQ) {
 	/* Pour reset une slide, il faut cacher les blocs réponses
 	Mettre le background color des propositions à #E1F1F3
 	Remettre le onclick des propositions à disponible (onclick à "chooseAnswer(this.id)") */
+	console.debug("Reset : "+idQ);
 	
 	// Cacher les blocs réponses
 	var bad_block = document.getElementById(idQ+"_ab");
 	var good_block = document.getElementById(idQ+"_ag");
+
+	bad_block.style.display = "none";
+	good_block.style.display = "none";
+
+	// Background des propositions à #E1F1F3 et onclick
+	var props = document.getElementById(idQ).children;
+
+	for(var i = 0; i < props.length; i++) {
+		console.debug("Node : "+props[i].id);
+		if(props[i].nodeName == "LI") {
+			console.debug("Background et onclick");
+			document.getElementById(props[i].id).style.backgroundColor = "#E1F1F3";
+			document.getElementById(props[i].id).style.pointerEvents = "auto";
+		}
+	}
+
+	document.getElementById(idQ+"_b").style.pointerEvents = "auto";
+
 }
 
 function chooseAnswer (prop_id) {
@@ -71,11 +99,13 @@ function answerQuestions(prop_id) {
 			var answer = childs[i].id;
 			answer = answer[3]+answer[4];
 		}
-		document.getElementById(childs[i].id).onclick = "false";
+		document.getElementById(childs[i].id).style.pointerEvents = "none";
 	}
 
 	var bad_bloc = document.getElementById(question+"_ab");
 	var good_bloc = document.getElementById(question+"_ag");
+	var good_arrow = document.getElementById(question+"_ag_button");
+	var bad_arrow = document.getElementById(question+"_ab_button");
 
 	function appear(bloc) {
 		bloc.style.opacity = 0;
@@ -91,16 +121,24 @@ function answerQuestions(prop_id) {
 			x: -20
 		});
 	}
+
+	function animContinue (arrow) {
+		for(var i = 0; i < 5; i++) {
+			$(arrow).transition({ scale: 1.5 }, 650);
+			$(arrow).transition({ scale: 1 }, 650);
+		}
+	}
 	
 	switch(question) {
 		case "q1":
 			switch(answer) {
 				// Bonne réponse
 				case "p3":
-					bad_bloc.style.display = "none";
 					disappear(bad_bloc);
+					bad_bloc.style.display = "none";
 					good_bloc.style.display = "table";
 					appear(good_bloc);
+					animContinue(good_arrow);
 					break;
 
 				// Mauvaises réponses
@@ -108,8 +146,9 @@ function answerQuestions(prop_id) {
 				case "p2":
 					bad_bloc.style.display = "table";
 					appear(bad_bloc);
-					good_bloc.style.display = "none";
 					disappear(good_bloc);
+					good_bloc.style.display = "none";
+					animContinue(bad_arrow);
 					break;
 			}
 			break;
@@ -118,10 +157,11 @@ function answerQuestions(prop_id) {
 			switch(answer) {
 				// Bonne réponse
 				case "p2":
-					bad_bloc.style.display = "none";
 					disappear(bad_bloc);
+					bad_bloc.style.display = "none";
 					good_bloc.style.display = "table";
 					appear(good_bloc);
+					animContinue(good_arrow);
 					break;
 
 				// Mauvaises réponses
@@ -129,8 +169,9 @@ function answerQuestions(prop_id) {
 				case "p3":
 					bad_bloc.style.display = "table";
 					appear(bad_bloc);
-					good_bloc.style.display = "none";
 					disappear(good_bloc);
+					good_bloc.style.display = "none";
+					animContinue(bad_arrow);
 					break;
 			}
 			break;
@@ -139,10 +180,11 @@ function answerQuestions(prop_id) {
 			switch(answer) {
 				// Bonne réponse
 				case "p1":
-					bad_bloc.style.display = "none";
 					disappear(bad_bloc);
+					bad_bloc.style.display = "none";
 					good_bloc.style.display = "table";
 					appear(good_bloc);
+					animContinue(good_arrow);
 					break;
 
 				// Mauvaises réponses
@@ -150,8 +192,9 @@ function answerQuestions(prop_id) {
 				case "p3":
 					bad_bloc.style.display = "table";
 					appear(bad_bloc);
-					good_bloc.style.display = "none";
 					disappear(good_bloc);
+					good_bloc.style.display = "none";
+					animContinue(bad_arrow);
 					break;
 			}
 			break;
@@ -164,6 +207,7 @@ function answerQuestions(prop_id) {
 					disappear(bad_bloc);
 					good_bloc.style.display = "table";
 					appear(good_bloc);
+					animContinue(good_arrow);
 					break;
 
 				// Mauvaises réponses
@@ -173,6 +217,7 @@ function answerQuestions(prop_id) {
 					appear(bad_bloc);
 					good_bloc.style.display = "none";
 					disappear(good_bloc);
+					animContinue(bad_arrow);
 					break;
 			}
 			break;
