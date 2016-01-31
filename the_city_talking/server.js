6a8146d8e801cf23f17922971d0121e2d4cb4dc4
Cobrand <cobrandw@gmail.com>
2016-01-31 16:20:23 +0100

    Merge test-cobrand into master

--- the_city_talking/server.js
+++ the_city_talking/server.js
@@ -1,3 +1,92 @@
+'use strict';
+
+const fs = require('fs');
+const moment = require('moment');
+
+function truncate(value){
+	return Math.round(100 * value) / 100 ;
+}
+
+function averageADay(oneDay){
+	var averageOneDay = {"NO":0,"NO2":0,"PM25":0} ;
+	var countParticles = {"NO":0,"NO2":0,"PM25":0} ;
+	oneDay.forEach((v,hour) => {
+		for ( let particle in v){
+			if (!isNaN(v[particle])){
+				countParticles[particle] += 1 ;
+				averageOneDay[particle] += v[particle] ;
+			}
+		}
+	});
+	averageOneDay["NO2"] = truncate(averageOneDay["NO2"] / countParticles["NO2"]) ;
+	averageOneDay["NO"] = truncate(averageOneDay["NO"] / countParticles["NO"]) ;
+	averageOneDay["PM25"] = truncate(averageOneDay["PM25"] / countParticles["PM25"]) ;
+
+	return averageOneDay ;
+}
+
+function makeAnAverageDay(multipleDays){
+	var array_data_oneDay = [];
+	var countParticles = [] ;
+	for (var i = 0 ; i < 24 ; i++){
+		array_data_oneDay.push({"NO":0,"NO2":0,"PM25":0});
+		countParticles.push({"NO":0,"NO2":0,"PM25":0});
+	}
+	multipleDays.forEach( (dataOneHour) => {
+		dataOneHour.hours.forEach((particlesOneHour,hour) => {
+			for (particle in particlesOneHour){
+				if (!isNaN(particlesOneHour[particle])){
+					array_data_oneDay[hour][particle] += particlesOneHour[particle];
+					countParticles[hour][particle] += 1 ;
+				}
+			}
+		});
+	});
+	array_data_oneDay.map((hourData,hour) => {
+		hourData["NO2"] = truncate( hourData["NO2"] / countParticles[hour]["NO2"] ) ;
+		hourData["NO"] = truncate( hourData["NO"] / countParticles[hour]["NO"] ) ;
+		hourData["PM25"] = truncate( hourData["PM25"] / countParticles[hour]["PM25"] ) ;
+	})
+	return array_data_oneDay ;
+}
+
+function processData(err,data,f,b,filterDay){
+	data = data.split('\n').map((line) => {
+		line = line.split(',');
+		return line ;
+	});
+	var data_filtered = {};
+	data.forEach((line) => {
+		var date = line[0];
+		data_filtered[date] = data_filtered[date] || {hours:[]};
+		data_filtered[date].hours.push({"NO":parseInt(line[8]),"NO2":parseInt(line[11]),"PM25":parseInt(line[20])});
+	})
+	var array_data_filtered = [] ;
+	for (var date in data_filtered){
+		if (date != ''){
+			data_filtered[date].date = moment(date,"DD-MM-YYYY");
+			array_data_filtered.push(data_filtered[date]);
+		}
+	}
+
+	array_data_filtered = array_data_filtered.filter(filterDay)
+
+	// b = TRUE if you want an average day (1 pt = 1 hour)
+	// b = FALSE if you want every day on a span (1pt = 1 day)
+	if (b){
+		f(makeAnAverageDay(array_data_filtered).map((value,index) => {
+				value["Hour"] = (index+1)+":00" ;
+				return value ;
+		}))
+	}else{
+		f(array_data_filtered.map((value) => {
+			var tmp = averageADay(value.hours) ;
+			tmp['Date'] = value.date.format("DD-MM-YYYY") ;
+			return tmp ;
+		}))
+	}
+}
+
 // Load usefull expressjs and nodejs objects / modules
 var express = require('express');
 var path = require('path');
@@ -5,6 +94,19 @@ var path = require('path');
 var app = express();
 
 // Minimum routing: serve static content from the html directory
+app.get('/json/:json',(req,res) => {
+	var result = /^(\d{2}\-\d{4})\.json$/.exec(req.params.json);
+	if (result == null){
+		res.status(404);
+		res.send("requete invalide");
+	}else{
+		let month = result[1] ;
+		fs.readFile(path.join(__dirname,'data/Centre_all_years.csv'),'utf8',(err,data) => {
+			processData(err,data,(e) => res.json(e),false,(obj) => obj.date.format("MM-YYYY") == result[1]);
+		});
+	}
+})
+
 app.use(express.static(path.join(__dirname, 'html')));
 
 // You can then add whatever routing code you need
--- the_city_talking/src/js/client.js
+++ the_city_talking/src/js/client.js
@@ -1,3 +1,46 @@
 const d3 = require('d3');
-let test = 2 ;
-alert(test);
+import "../scss/main.scss";
+
+function parseDay(d){
+	return {
+		"Hour":d.Hour,
+		"NO":+d.NO,
+		"NO2":+d.NO2,
+		"PM25":+d.PM25,
+	}
+}
+
+let width = 960,
+	height = 500;
+
+let y = d3.scale.linear()
+	.range([height, 0]);
+
+let chart = d3.select(".chart")
+	.attr("width", width)
+	.attr("height", height);
+
+d3.csv("data/average_sun.csv",parseDay,(err,data) => {
+	y.domain([0, d3.max(data, function(d) { return d.NO; })]);
+
+	let barWidth = width / data.length;
+
+	let bar = chart.selectAll("g")
+		.data(data)
+		.enter()
+		.append("g")
+		.attr("transform",(d, i) =>  "translate(" + i * barWidth + ",0)" );
+
+	bar.append("rect")
+		.attr("y", (d) => y(d.NO) )
+		.attr("height", (d) =>  height - y(d.NO))
+		.attr("width", barWidth - 1);
+	/*
+	bar.append("text")
+		.attr("x", barWidth / 2)
+		.attr("y", function(d) { return y(d.value) + 3; })
+		.attr("dy", ".75em")
+		.text(function(d) { return d.value; });
+	});
+	*/
+});
