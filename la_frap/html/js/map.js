fbe0b060fc2b60efd4a1c43a4df57589799110d6
Ambre Person <ambre.person@etu.univ-nantes.fr>
2016-01-31 16:08:31 +0100

    commit

--- la_frap/html/js/map.js
+++ la_frap/html/js/map.js
@@ -75,28 +75,28 @@ window.addEventListener('load', function () {
 
 
 	var jsonMarker = [
-		{"x":134.716,"y":330.95,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":142.93,"y":322.737,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":26.323,"y":330.95,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":67.387,"y":297.277,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":41.635,"y":222.281,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":58.95,"y":248.742,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":150.455,"y":151.125,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":58.95,"y":260.654,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":101.524,"y":223.309,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":109.123,"y":229.881,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":86.481,"y":255.727,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":120.008,"y":230.087,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":115.487,"y":219.817,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":99.538,"y":237.479,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":110.766,"y":240.15,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":238.695,"y":197.208,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":248.007,"y":200.7,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":240.615,"y":208.913,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":327.673,"y":199.878,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":232.514,"y":138.131,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":248.007,"y":47.732,"r":3.492,"c":"white","texte":"Radio 1"},
-		{"x":337.444,"y":103.58,"r":3.492,"c":"white","texte":"Radio 1"}
+		{"x":134.716,"y":330.95,"r":3.492,"c":"white","nom":"nom 1","texte":"texte 1"},
+		{"x":142.93,"y":322.737,"r":3.492,"c":"white","nom":"nom 2","texte":"texte 2"},
+		{"x":26.323,"y":330.95,"r":3.492,"c":"white","nom":"nom 3","texte":"texte 3"},
+		{"x":67.387,"y":297.277,"r":3.492,"c":"white","nom":"nom 4","texte":"texte 4"},
+		{"x":41.635,"y":222.281,"r":3.492,"c":"white","nom":"nom 5","texte":"texte 5"},
+		{"x":58.95,"y":248.742,"r":3.492,"c":"white","nom":"nom 6","texte":"texte 6"},
+		{"x":150.455,"y":151.125,"r":3.492,"c":"white","nom":"nom 7","texte":"texte 7"},
+		{"x":58.95,"y":260.654,"r":3.492,"c":"white","nom":"nom 8","texte":"texte 8"},
+		{"x":101.524,"y":223.309,"r":3.492,"c":"white","nom":"nom 9","texte":"texte 9"},
+		{"x":109.123,"y":229.881,"r":3.492,"c":"white","nom":"nom 10","texte":"texte 10"},
+		{"x":86.481,"y":255.727,"r":3.492,"c":"white","nom":"nom 11","texte":"texte 11"},
+		{"x":120.008,"y":230.087,"r":3.492,"c":"white","nom":"nom 12","texte":"texte 12"},
+		{"x":115.487,"y":219.817,"r":3.492,"c":"white","nom":"nom 13","texte":"texte 13"},
+		{"x":99.538,"y":237.479,"r":3.492,"c":"white","nom":"nom 14","texte":"texte 14"},
+		{"x":110.766,"y":240.15,"r":3.492,"c":"white","nom":"nom 15","texte":"texte 15"},
+		{"x":238.695,"y":197.208,"r":3.492,"c":"white","nom":"nom 16","texte":"texte 16"},
+		{"x":248.007,"y":200.7,"r":3.492,"c":"white","nom":"nom 17","texte":"texte 17"},
+		{"x":240.615,"y":208.913,"r":3.492,"c":"white","nom":"nom 18","texte":"texte 18"},
+		{"x":327.673,"y":199.878,"r":3.492,"c":"white","nom":"nom 19","texte":"texte 19"},
+		{"x":232.514,"y":138.131,"r":3.492,"c":"white","nom":"nom 20","texte":"texte 20"},
+		{"x":248.007,"y":47.732,"r":3.492,"c":"white","nom":"nom 21","texte":"texte 21"},
+		{"x":337.444,"y":103.58,"r":3.492,"c":"white","nom":"nom 22","texte":"texte 22"}
 
 	]
 
@@ -105,9 +105,8 @@ window.addEventListener('load', function () {
 										.enter()
 										.append("circle");
 
-	var div = d3.select("body")
-				.append("div")
-				.attr("class", "tooltip");
+	var div = d3.select("#tooltip");
+				
 
 	var markerAttributes = markers
            .attr("cx", function (d) {return d.x;})
@@ -119,12 +118,17 @@ window.addEventListener('load', function () {
            		markers.attr("r", function (d) {return d.r;})
 					   .style("fill", function (d) {return d.c;});
            })
-           .on("click", function (d) {
+           /*.on("click", function (d) {
            		div.html(d.texte)
            		   .style("left",(d3.event.pageX) + "px")
            		   .style("top",(d3.event.pageY+10) + "px")
            		   .style("visibility","visible");
-           })
+           })*/
+		   .on("click", function (d) {
+		   		div.html("<h1>"+d.nom+"</h1>"+"<p>"+d.texte+"</p>")
+		   		   .style("visibility","visible");
+
+		   })
            .on("mouseout", function (d) {
            		div.style("visibility","hidden");
            		d.c = "white";
