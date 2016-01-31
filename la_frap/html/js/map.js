419eab556acb0436a65f700cff4bfdcd1a104548
Ambre Person <ambre.person@etu.univ-nantes.fr>
2016-01-31 17:13:54 +0100

    ajout des descriptions des radios

--- la_frap/html/js/map.js
+++ la_frap/html/js/map.js
@@ -59,8 +59,9 @@ window.addEventListener('load', function () {
 		{"n":"Vendee","r":5,"a":6,"s":4,"b":62,"sc":9},				
 		{"n":"Loire Atlantique","r":3,"a":4,"s":3,"b":58,"sc":5},
 		{"n":"Maine et Loire","r":4,"a":2,"s":6,"b":53,"sc":2},
-		{"n":"Sarthe","r":2,"a":4,"s":2,"b":65,"sc":0},
 		{"n":"Mayenne","r":8,"a":3,"s":5,"b":50,"sc":7},
+		{"n":"Sarthe","r":2,"a":4,"s":2,"b":65,"sc":0}
+
 
 	]
 
@@ -75,28 +76,27 @@ window.addEventListener('load', function () {
 
 
 	var jsonMarker = [
-		{"x":134.716,"y":330.95,"r":3.492,"c":"white","nom":"nom 1","texte":"texte 1"},
-		{"x":142.93,"y":322.737,"r":3.492,"c":"white","nom":"nom 2","texte":"texte 2"},
-		{"x":26.323,"y":330.95,"r":3.492,"c":"white","nom":"nom 3","texte":"texte 3"},
-		{"x":67.387,"y":297.277,"r":3.492,"c":"white","nom":"nom 4","texte":"texte 4"},
-		{"x":41.635,"y":222.281,"r":3.492,"c":"white","nom":"nom 5","texte":"texte 5"},
-		{"x":58.95,"y":248.742,"r":3.492,"c":"white","nom":"nom 6","texte":"texte 6"},
-		{"x":150.455,"y":151.125,"r":3.492,"c":"white","nom":"nom 7","texte":"texte 7"},
-		{"x":58.95,"y":260.654,"r":3.492,"c":"white","nom":"nom 8","texte":"texte 8"},
-		{"x":101.524,"y":223.309,"r":3.492,"c":"white","nom":"nom 9","texte":"texte 9"},
-		{"x":109.123,"y":229.881,"r":3.492,"c":"white","nom":"nom 10","texte":"texte 10"},
-		{"x":86.481,"y":255.727,"r":3.492,"c":"white","nom":"nom 11","texte":"texte 11"},
-		{"x":120.008,"y":230.087,"r":3.492,"c":"white","nom":"nom 12","texte":"texte 12"},
-		{"x":115.487,"y":219.817,"r":3.492,"c":"white","nom":"nom 13","texte":"texte 13"},
-		{"x":99.538,"y":237.479,"r":3.492,"c":"white","nom":"nom 14","texte":"texte 14"},
-		{"x":110.766,"y":240.15,"r":3.492,"c":"white","nom":"nom 15","texte":"texte 15"},
-		{"x":238.695,"y":197.208,"r":3.492,"c":"white","nom":"nom 16","texte":"texte 16"},
-		{"x":248.007,"y":200.7,"r":3.492,"c":"white","nom":"nom 17","texte":"texte 17"},
-		{"x":240.615,"y":208.913,"r":3.492,"c":"white","nom":"nom 18","texte":"texte 18"},
-		{"x":327.673,"y":199.878,"r":3.492,"c":"white","nom":"nom 19","texte":"texte 19"},
-		{"x":232.514,"y":138.131,"r":3.492,"c":"white","nom":"nom 20","texte":"texte 20"},
-		{"x":248.007,"y":47.732,"r":3.492,"c":"white","nom":"nom 21","texte":"texte 21"},
-		{"x":337.444,"y":103.58,"r":3.492,"c":"white","nom":"nom 22","texte":"texte 22"}
+		{"x":134.716,"y":330.95,"r":3.492,"c":"white","nom":"RCF Vendée","texte":"RCF est  une radio chrétienne locale d’information et de culture qui couvre l’ensemble de la Vendée."},
+		{"x":142.93,"y":322.737,"r":3.492,"c":"white","nom":"Graffiti Urban","texte":"Graffiti Urban Radio constitue un véritable forum de libre expression pour la population locale, grâce à la diffusion quotidienne de programmes animés par ses habitants bénévoles et sa politique d'accessibilité à l'antenne."},
+		{"x":26.323,"y":330.95,"r":3.492,"c":"white","nom":"Neptune FM","texte":"Neptune FM, émettant de l’Ile D’Yeu, propose une programmation pop/rock axée sur la nouvelle scène francophone, les nouveautés internationales et les classiques du genre mais ne néglige pas pour autant les autres styles musicaux."},
+		{"x":67.387,"y":297.277,"r":3.492,"c":"white","nom":"Nov FM","texte":" Nov FM dispose depuis 10 ans du même leitmotiv : valoriser tous les acteurs locaux tant par le tissu associatif que la vie économique."},
+		{"x":41.635,"y":222.281,"r":3.492,"c":"white","nom":"La Tribu","texte":"La Tribu n’est pas qu’une radio c’est aussi un atelier jeunesse. L’équipe de la radio est mixte, composée d’animateurs jeunesse et de journalistes, permettant ainsi d’offrir aux collégiens et lycéens un espace de parole quotidien."},
+		{"x":110.455,"y":186.125,"r":3.492,"c":"white","nom":"Atlantis FM","texte":"Atlantis FM diffuse un concept exclusif à destination des 20-60 ans, étudiants, demandeurs d'emploi comme actifs avec une thématique antenne sur l’emploi, la formation et le développement économique local."},
+		{"x":58.95,"y":260.654,"r":3.492,"c":"white","nom":"Radio Chrono","texte":"Radio associative créée en 1981, Radio Chrono permet à ses auditeurs de découvrir des artistes locaux, peu ou pas diffusés sur les radios commerciales. Généraliste, elle est orientée vers la vie locale et associative du Pays de Retz et du Nord Vendée."},
+		{"x":101.524,"y":223.309,"r":3.492,"c":"white","nom":"Euradionantes","texte":"Euradionantes, c’est l’actualité européenne à plusieurs voix, en plusieurs langues avec un son résolument européen et un credo : l’info locale européenne."},
+		{"x":109.123,"y":229.881,"r":3.492,"c":"white","nom":"Radio Prun'","texte":"Prun’ est la radio jeune faite par 250 bénévoles, pour tous. Au programme : musique, culture, société, politique, sciences, web… Toutes les thématiques sont abordées pour plaire au plus grand nombre."},
+		{"x":116.481,"y":265.727,"r":3.492,"c":"white","nom":"NTI","texte":"NTI est une radio associative musicale qui se consacre aux sons « mix  et  dance »."},
+		{"x":120.008,"y":230.087,"r":3.492,"c":"white","nom":"SUN","texte":"SUN est une radio locale qui s’adresse à tous les habitants de Nantes et sa région. Elle s’engage à faire vivre les grands événements nantais et propose une programmation musicale éclectique et de l’information avec une forte implication locale."},
+		{"x":115.487,"y":219.817,"r":3.492,"c":"white","nom":"Jet FM","texte":"Jet FM propose une antenne locale éclectique, passionnée, exigeante, engagée, militante et ouverte avec au programme de la musique en tous genres sélectionnées hors des autoroutes commerciales et sur les petits chemins de la créativité."},
+		{"x":99.538,"y":237.479,"r":3.492,"c":"white","nom":"Fidelité","texte":"Fidelité est une radio chrétienne qui produit et diffuse des reportages et des émissions réalisés dans tous les domaines de la société, qui informent sur les événements locaux, petits et grands. "},
+		{"x":110.766,"y":240.15,"r":3.492,"c":"white","nom":"AlterNantes","texte":"Radio pluraliste, humaniste et multilingue, Alternantes FM lutte contre toute forme d’exclusion,  d’intolérance et de racisme. Son antenne offre un moyen d’expression à celles et ceux qui souhaitent promouvoir leurs projets et actions."},
+		{"x":238.695,"y":197.208,"r":3.492,"c":"white","nom":"Radio G!","texte":"Radio G! est une radio de proximité guidée par la diversité et l'ouverture d'esprit. Sa grille des programmes regroupe plus d'une cinquantaine d'émissions, musicales ou thématiques, animées par une centaine de bénévoles de tous âges et de tous horizons."},
+		{"x":248.007,"y":200.7,"r":3.492,"c":"white","nom":"Radio Campus","texte":"Radio Campus Angers se fait l’écho du monde étudiant angevin en proposant de l’information, de la culture et de la musique pour tous les goûts mais toujours au cœur de l’actualité."},
+		{"x":240.615,"y":208.913,"r":3.492,"c":"white","nom":"RCF Anjou","texte":"RCF Anjou est une radio chrétienne locale généraliste, elle diffuse 24h/24 des émissions dont les thèmes sont très variés : information, musique, culture et reportages."},
+		{"x":327.673,"y":199.878,"r":3.492,"c":"white","nom":"RPSFM","texte":"RPSFM est une radio musicale rurale qui propose aux auditeurs des programmes qui se démarquent, au-delà de toutes idéologies."},
+		{"x":232.514,"y":138.131,"r":3.492,"c":"white","nom":"L'Autre Radio","texte":"L'Autre Radio offre une alternative aux auditeurs grâce à une programmation éclectique et novatrice. Elle se fait entendre avec une programmation musicale de qualité et de l'information internationale, nationale  et locale dans des émissions quotidiennes."},
+		{"x":248.007,"y":47.732,"r":3.492,"c":"white","nom":"Fidelité Mayenne","texte":"Fidélité Mayenne radio se met au service des associations, des groupements mais aussi des mouvements de l’église locale, particulièrement attentive aux plus démunis."},
+		{"x":337.444,"y":103.58,"r":3.492,"c":"white","nom":"RCF Le Mans","texte":"RCF Le Mans est une radio locale, généraliste et chrétienne  destinée à un public très large, quelles que soient ses convictions."}
 
 	]
 
@@ -113,8 +113,8 @@ window.addEventListener('load', function () {
            .attr("cy", function (d) {return d.y;})
            .attr("r", function (d) {return d.r;})
            .on("mouseover", function (d) {
-           		d.c = "red";
-           		d.r = 4.3;
+           		d.c = "#ff2800";
+           		d.r = 4.8;
            		markers.attr("r", function (d) {return d.r;})
 					   .style("fill", function (d) {return d.c;});
            })
