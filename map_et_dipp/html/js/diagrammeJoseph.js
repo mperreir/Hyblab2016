cfe04d05946f00faf6045984dc2e8ba8c9ebb498
Maxime PINEAU <maxime.pineau3@etu.univ-nantes.fr>
2016-02-02 18:55:11 +0100

    slide3: joseph svg responsive test (fail)

--- map_et_dipp/html/js/diagrammeJoseph.js
+++ map_et_dipp/html/js/diagrammeJoseph.js
@@ -136,8 +136,16 @@ donneesActuelles.forEach(function(element, index, tableau) {
     var idJauge = infoElement.idJauge;
     
     var baliseJauge = d3.select("#diagrammeJoseph").append("div").attr("class", "baliseJauge"); // col-xs-1
-    var svgJauge = baliseJauge.append("svg").attr("id", idJauge).attr("width", 100).attr("height", 150);
+    var svgJauge = baliseJauge.append("svg").attr("id", idJauge);//.attr("width", 100).attr("height", 150);
+    //svgJauge.attr("viewBox", "90 -80 100 280");
+    
     svgJauge.attr("viewBox", "0 0 100 150");
+    /*
+    svgJauge.attr({
+        "xmlns": "http://www.w3.org/2000/svg",
+        "version": "1.1"
+    });
+    */
     // <svg id="fillgauge6" width="19%" height="300" onclick="gauge6.update(NewValue());"></svg>
     
     var config = liquidFillGaugeDefaultSettings();
@@ -170,7 +178,7 @@ donneesActuelles.forEach(function(element, index, tableau) {
     
     // ajout de l'image dans la jauge
     // <image x="200" y="200" width="100px" height="100px" xlink:href="myimage.png"> 
-    svgJauge.select("g").append("image").attr("x", 0).attr("y", 0).attr("xlink:href", infoElement.cheminImage).attr("width", 100).attr("height", 100);
+    //svgJauge.select("g").append("image").attr("x", 0).attr("y", 0).attr("xlink:href", infoElement.cheminImage).attr("width", 100).attr("height", 100);
 });
 
 
