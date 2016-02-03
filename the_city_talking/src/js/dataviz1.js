7ed2ad6316811572911d21086a4c0e0868438678
Andrès Franco <andres.franco@etu.univ-nantes.fr>
2016-02-03 17:21:54 +0100

    Air Quality as scale

--- the_city_talking/src/js/dataviz1.js
+++ the_city_talking/src/js/dataviz1.js
@@ -2,7 +2,7 @@ const d3 = require('d3');
 d3.tip = require('d3-tip');
 const utils = require("./utils.js");
 
-let tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return formatDateShow(d.Date) + "<br><strong>NO2 = " + d.NO2 + "</strong>"; });
+let tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return formatDateShow(d.Date) + "<br><strong>Pollution Level = " + Math.floor(d.Level*10) / 10 + "</strong><br>NO2 = "+d.NO2; });
 
 var o_width=950,
 	o_height=400;
@@ -20,7 +20,8 @@ d3.csv("per_month_kerbside.csv",(err,data_csv) => {
             "Date":formatDate.parse(datum.Date),
             "NO":+datum.NO,
             "NO2":+datum.NO2,
-            "PM25":+datum.PM25
+            "PM25":+datum.PM25,
+			"Level":+datum.NO2 / 25
         }
     });
 
@@ -32,6 +33,7 @@ d3.csv("per_month_kerbside.csv",(err,data_csv) => {
 	var max_NO2 = 250;
 
     let scale_y = d3.scale.linear().range([height,0]).domain([min_NO2,max_NO2]);
+	let scale_y_aq = d3.scale.linear().range([height,0]).domain([min_NO2/25,max_NO2/25]);
     let scale_x = d3.time.scale().range([0,width]).domain(d3.extent(data,function(d){return d.Date;}))
 
     var xAxis = d3.svg.axis()
@@ -39,7 +41,7 @@ d3.csv("per_month_kerbside.csv",(err,data_csv) => {
 	    .orient("bottom");
 
 	var yAxis = d3.svg.axis()
-	    .scale(scale_y)
+	    .scale(scale_y_aq)
 	    .orient("left");
 
 	chart.append("g")
@@ -73,23 +75,23 @@ d3.csv("per_month_kerbside.csv",(err,data_csv) => {
     var seuilmoyen=(100-offsets(110,min_NO2,max_NO2))+"%";
     var seuilmauvais=(100-offsets(200,min_NO2,max_NO2))+"%";
 
-   	chart.append("linearGradient")                
-        .attr("id", "line-gradient-d1")            
-        .attr("gradientUnits", "userSpaceOnUse")    
-        .attr("x1", 0).attr("y1", 0)         
-        .attr("x2", 0).attr("y2", height)      
-    .selectAll("stop")                      
-        .data([                             
-            {offset: "0%", color: "rgb(249,98,90)"},       
-            {offset: seuilmauvais, color: "rgb(249,98,90)"},  
-            {offset: seuilmauvais, color: "rgb(255,167,136)"},        
-            {offset: seuilmoyen, color: "rgb(255,167,136)"},        
-            {offset: seuilmoyen, color: "rgb(28,247,168)"},    
-            {offset: "100%", color: "rgb(28,247,168)"}    
-        ])                  
-    .enter().append("stop")         
-        .attr("offset", function(d) { return d.offset; })   
-        .attr("stop-color", function(d) { return d.color; });  
+   	chart.append("linearGradient")
+        .attr("id", "line-gradient-d1")
+        .attr("gradientUnits", "userSpaceOnUse")
+        .attr("x1", 0).attr("y1", 0)
+        .attr("x2", 0).attr("y2", height)
+    .selectAll("stop")
+        .data([
+            {offset: "0%", color: "rgb(249,98,90)"},
+            {offset: seuilmauvais, color: "rgb(249,98,90)"},
+            {offset: seuilmauvais, color: "rgb(255,167,136)"},
+            {offset: seuilmoyen, color: "rgb(255,167,136)"},
+            {offset: seuilmoyen, color: "rgb(28,247,168)"},
+            {offset: "100%", color: "rgb(28,247,168)"}
+        ])
+    .enter().append("stop")
+        .attr("offset", function(d) { return d.offset; })
+        .attr("stop-color", function(d) { return d.color; });
 
     var path = chart.append("path")
     path.datum(data)
