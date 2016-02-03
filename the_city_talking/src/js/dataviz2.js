3dd485706bec6d6d707bc1f50874e2d5401be558
Andrès Franco <andres.franco@etu.univ-nantes.fr>
2016-02-03 12:58:26 +0100

    button change 2nd dataviz

--- the_city_talking/src/js/dataviz2.js
+++ the_city_talking/src/js/dataviz2.js
@@ -13,8 +13,19 @@ let padding_y = 20 ;
 let scale_y = d3.scale.linear().range([height,0]).domain([0,utils.max_NO2]);
 let scale_x = d3.scale.ordinal().rangePoints([0, width],0.5);
 
-let months_demo = ["01-2014","02-2014","03-2014"];
-let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+let months = [
+	{name:'J',uri:'01'},
+	{name:'F',uri:'02'},
+	{name:'M',uri:'03'},
+	{name:'A',uri:'04'},
+	{name:'M',uri:'05'},
+	{name:'J',uri:'06'},
+	{name:'J',uri:'07'},
+	{name:'A',uri:'09'},
+	{name:'S',uri:'08'},
+	{name:'O',uri:'10'},
+	{name:'N',uri:'11'},
+	{name:'D',uri:'12'}];
 let years = [2010,2011,2012,2013,2014,2015]
 let facilities = [
 	{name:"Centre",uri:"json_centre/"},
@@ -24,25 +35,6 @@ let current_year = 0 ;
 let current_facility = 0 ;
 
 let tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return d.NO2; });
-
-months.forEach((e,i) => {
-	var button = document.createElement("button");
-	button.innerHTML = e ;
-	button.classList.add("dataviz2_button");
-	document.getElementById("buttons_dataviz2_months").appendChild(button);
-})
-years.forEach((e,i) => {
-	var button = document.createElement("button");
-	button.innerHTML = e ;
-	button.classList.add("dataviz2_button");
-	document.getElementById("buttons_dataviz2_years").appendChild(button);
-})
-facilities.forEach((e,i) => {
-	var button = document.createElement("button");
-	button.innerHTML = e.name ;
-	button.classList.add("dataviz2_button");
-	document.getElementById("buttons_dataviz2_facilities").appendChild(button);
-})
 
 function getPollutionCategory(value){
 	for (var i = 1 ; i < utils.pollution_values.length ; i++){
@@ -64,9 +56,14 @@ chart.selectAll('.bg_square').data(utils.pollution_values.filter((d) => (d != ut
 	.attr('height',(d,i) => scale_y(utils.max_NO2 - (utils.pollution_values[i+1]-utils.pollution_values[i])))
 	.each(function (d,i){d3.select(this).classed(utils.pollution_classes[i],true)});
 
-function loopMonth(){
-
-	d3.json("json_kerbside/"+months_demo[current_month]+".json",(err,data) => {
+function updateChart(month_n = null ,year_n = null ,facility_n = null ,loop = true){
+	facility_n = facility_n || current_facility ;
+	month_n = month_n || current_month ;
+	year_n = year_n || current_year ;
+	if (loop !== true){
+		loopMonth = () => {} ;
+	}
+	d3.json(facilities[facility_n].uri+months[month_n].uri+"-"+years[year_n]+".json",(err,data) => {
 		scale_x = scale_x.domain(data.map((dataOneHour) => dataOneHour.Hour)) ;
 		//scale_y = scale_y.domain(d3.extent(data.map((dataOneHour) => dataOneHour.NO2)));
 		var circles = chart.selectAll("circle").data(data);
@@ -92,9 +89,45 @@ function loopMonth(){
 					.classed(utils.pollution_classes.join(' '),false)
 					.classed(getPollutionCategory(d.NO2),true)
 			});
-		current_month = (current_month + 1) % months_demo.length ;
-		setTimeout(loopMonth,2000);
 	});
 }
 
-loopMonth();
+function selectMonth(n){
+	current_month = n ;
+	updateChart();
+}
+
+function selectYear(n){
+	current_year = n ;
+	updateChart();
+}
+
+function selectFacility(n){
+	current_facility = n ;
+	updateChart();
+}
+
+updateChart();
+
+months.forEach((e,i) => {
+	var button = document.createElement("button");
+	button.innerHTML = e.name ;
+	button.classList.add("dataviz2_button");
+	button.addEventListener("click",selectMonth.bind(null,i));
+	document.getElementById("buttons_dataviz2_months").appendChild(button);
+})
+
+years.forEach((e,i) => {
+	var button = document.createElement("button");
+	button.innerHTML = e ;
+	button.classList.add("dataviz2_button");
+	button.addEventListener("click",selectYear.bind(null,i));
+	document.getElementById("buttons_dataviz2_years").appendChild(button);
+})
+facilities.forEach((e,i) => {
+	var button = document.createElement("button");
+	button.innerHTML = e.name ;
+	button.classList.add("dataviz2_button");
+	button.addEventListener("click",selectFacility.bind(null,i));
+	document.getElementById("buttons_dataviz2_facilities").appendChild(button);
+});
