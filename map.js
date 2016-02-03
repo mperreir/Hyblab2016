b93d4f76c5580dd2c9c50d8cbe1c72c524e5a209
Kunli <dengkunli@foxmail.com>
2016-02-03 10:04:26 +0100

    remove osm and add zoom control

--- map.js
+++ map.js
@@ -430,22 +430,23 @@
         onEachFeature: MsoaOnEachFeature
     });
 
+    /*
     var osm = new L.TileLayer(
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
             minZoom: 11,
             maxZoom: 18,
             attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         }
-    );
+    );*/
 
     var map = L.map('map', {
         center: [53.85, -2.7],
         zoom: 11,
-        layers: [osm, topoMsoaLayer], // Only Add default layers here
+        layers: [topoMsoaLayer], // Only Add default layers here
         minZoom: 11,
         maxZoom: 16,
         maxBounds: topoMsoaLayer.getBounds(),
-        zoomControl: false
+        zoomControl: true
     });
 
     map.on('zoomend', function(e) {
