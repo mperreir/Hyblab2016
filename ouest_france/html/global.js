0217867e1fa341e672a8a9892c98f15e15d65305
Valars <romaingar.garnier@gmail.com>
2016-02-03 12:55:01 +0100

    Maj requete

--- ouest_france/html/global.js
+++ ouest_france/html/global.js
@@ -9,8 +9,6 @@ $(document).ready(function() {
 	requestGenerateChartDefilement("menage");
 	requestGenerateChartDefilement("parc");
 	requestGenerateChartDefilement("carburant");
-
-	
 });
 
 /********* TEST ***********/
