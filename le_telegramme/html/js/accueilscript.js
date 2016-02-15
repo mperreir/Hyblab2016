6eff7e62acc6fe19290e8558d41c94e995de900b
Becko Junior Camara <becko-junior.camara@etu.univ-nantes.fr>
2016-02-15 20:13:18 +0100

    Page d'intro

---
+++ le_telegramme/html/js/accueilscript.js
@@ -0,0 +1,24 @@
+$(document).ready( function () {
+  var time = 10;
+  var inter = 1;
+  var progress = $("#barre_progression");
+
+  var animate = function () {
+    progress.val(progress.val() + progress.attr("max")/time);
+    if (progress.val() < 100) {
+      setTimeout(animate, 1000)
+    }
+    else {
+      progress.val(progress.attr("max"));
+    }
+  }
+  setTimeout(animate, 3000);
+
+/************************************************************************************/
+  $('.js-scrollTo').on('click', function() {
+			var page = $(this).attr('href');
+			var speed = 1000;
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
+});
