9de6cb620cbf05404f1d56771020c4b095c4ba80
Antoine Vaillant <a.vaillant.sn@gmail.com>
2016-02-19 17:35:18 +0100

    fix

--- le_telegramme/html/js/comment_random.js
+++ le_telegramme/html/js/comment_random.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
 	$("#comments_1").on('click', function() {
 		console.log('clic comments_1');
 		$.get('data/comments.json', function(data) { // JQuery HTTP GET request to the server
+			//If the request is OK :
 			try {
+			  // var comments = JSON.parse(data); // parse json string answer to get a javascript object
 			  var avis = false;
 			  var rand = 0;
 			  while(!avis){
@@ -22,9 +24,11 @@ $(document).ready(function() {
 	});
 
 	$("#comments_2").on('click', function() {
-		console.log('clic comments_2');
+		console.log('clic comments_1');
 		$.get('data/comments.json', function(data) { // JQuery HTTP GET request to the server
+			//If the request is OK :
 			try {
+			  // var comments = JSON.parse(data); // parse json string answer to get a javascript object
 			  var souvenir = false;
 			  var rand = 0;
 			  while(!souvenir){
--- le_telegramme/html/js/dynamic_panels.js
+++ le_telegramme/html/js/dynamic_panels.js
@@ -9,12 +9,12 @@ $(document).ready(function() {
   //Different IDs of the div we're displaying / hiding in jQuery
   var arr = ['#ca_pousse_edito', '#ca_pousse_sondage', '#ca_pousse_photos',
              '#eclosions_edito', '#eclosions_sondage', '#eclosions_photos',
-             '#quartiers_edito', '#quartiers_sondage', '#quartiers_photos', "#credits"];
+             '#quartiers_edito', '#quartiers_sondage', '#quartiers_photos'];
 
   var tabs = ["#submenu1", "#submenu2", "#submenu3"];
 
   //Initialization of the page
-  for(var i=0; i < 10; i++){
+  for(var i=0; i < 9; i++){
     $(arr[i]).hide();
   }
   $(arr[0]).show("slow");
@@ -25,7 +25,7 @@ $(document).ready(function() {
   $("#menu1").click( function() {
     console.log('clic menu1');
     $("#right iframe").attr("src", "map.html");
-    $("#submenu").show("fast");
+
     $("#submenu li").css("border-right", "2px solid #F7931E");
 
     switch(currentNav){
@@ -59,22 +59,12 @@ $(document).ready(function() {
         $("#submenu3").removeClass("noactive_b");
         $(this).addClass("active");
         break;
-      case 3:
-        $("#menu4").removeClass("active");
-        $(tabs[currentTab]).removeClass("active_b");
-        
-        //clean all the tabs
-        $("#submenu1").removeClass("noactive_b");
-        $("#submenu2").removeClass("noactive_b");
-        $("#submenu3").removeClass("noactive_b");
-        $(this).addClass("active");
-        break;        
     }
 
     //we update the current tab selected in the main navbar
     currentNav = 0;
     //hiding all the divs
-    for(var i=0; i < 10; i++){
+    for(var i=0; i < 9; i++){
       $(arr[i]).hide();
     }
 
@@ -97,7 +87,7 @@ $(document).ready(function() {
     console.log('clic menu2');
 
     $("#right iframe").attr("src", "map2.html");
-    $("#submenu").show("fast");
+
     $("#submenu li").css("border-right", "2px solid #FF605E");
 
     switch(currentNav){
@@ -131,20 +121,10 @@ $(document).ready(function() {
         $("#submenu3").removeClass("noactive_b");
         $(this).addClass("active");
         break;
-      case 3:
-        $("#menu4").removeClass("active");
-        $(tabs[currentTab]).removeClass("active_b");
-        
-        //clean all the tabs
-        $("#submenu1").removeClass("noactive_b");
-        $("#submenu2").removeClass("noactive_b");
-        $("#submenu3").removeClass("noactive_b");
-        $(this).addClass("active");
-        break;        
     }
 
     currentNav = 1;
-    for(var i=0; i < 10; i++){
+    for(var i=0; i < 9; i++){
       $(arr[i]).hide();
     }
 
@@ -165,7 +145,6 @@ $(document).ready(function() {
 
     $("#right iframe").attr("src", "map3.html");
     $("#submenu li").css("border-right", "2px solid #42B69E");
-    $("#submenu").show("fast");
 
     switch(currentNav){
       case 0:
@@ -198,20 +177,10 @@ $(document).ready(function() {
         $("#submenu3").removeClass("noactive_b");
         $(this).addClass("active");
         break;
-      case 3:
-        $("#menu4").removeClass("active");
-        $(tabs[currentTab]).removeClass("active_b");
-        
-        //clean all the tabs
-        $("#submenu1").removeClass("noactive_b");
-        $("#submenu2").removeClass("noactive_b");
-        $("#submenu3").removeClass("noactive_b");
-        $(this).addClass("active");
-        break;
     }
 
     currentNav = 2;
-    for(var i=0; i < 10; i++){
+    for(var i=0; i < 9; i++){
       $(arr[i]).hide();
     }
 
@@ -226,78 +195,12 @@ $(document).ready(function() {
   });
 
 
-  $("#menu4").click( function() {
-    console.log('clic menu4');
-
-    $("#right iframe").attr("src", "map4.html");
-    $("#submenu li").css("border-right", "2px solid #42B69E");
-    $("#submenu").hide();
-
-    switch(currentNav){
-      case 0:
-        $("#menu1").removeClass("active");
-        $(tabs[currentTab]).removeClass("active_j");
-
-        //clean all the tabs
-        $("#submenu1").removeClass("noactive_j");
-        $("#submenu2").removeClass("noactive_j");
-        $("#submenu3").removeClass("noactive_j");
-        // $(this).addClass("active");
-        break;
-      case 1:
-        $("#menu2").removeClass("active");
-        $(tabs[currentTab]).removeClass("active_r");
-        
-        //clean all the tabs
-        $("#submenu1").removeClass("noactive_r");
-        $("#submenu2").removeClass("noactive_r");
-        $("#submenu3").removeClass("noactive_r");
-        // $(this).addClass("active");
-        break;
-      case 2:
-        $("#menu3").removeClass("active");
-        $(tabs[currentTab]).removeClass("active_b");
-        
-        //clean all the tabs
-        $("#submenu1").removeClass("noactive_b");
-        $("#submenu2").removeClass("noactive_b");
-        $("#submenu3").removeClass("noactive_b");
-        // $(this).addClass("active");
-        break;
-      case 3:
-        $("#menu4").removeClass("active");
-        $(tabs[currentTab]).removeClass("active_b");
-        
-        //clean all the tabs
-        $("#submenu1").removeClass("noactive_b");
-        $("#submenu2").removeClass("noactive_b");
-        $("#submenu3").removeClass("noactive_b");
-        // $(this).addClass("active");
-        break;
-    }
-
-    currentNav = 3;
-    for(var i=0; i < 10; i++){
-      $(arr[i]).hide();
-    }
-
-    $("#credits").show("slow");
-
-    currentTab=0;
-    $(tabs[currentTab]).addClass("active_b");
-    $("#submenu2").addClass("noactive_b");
-    $("#submenu3").addClass("noactive_b");
-
-    $("#middle").css("background-color","#B4CD36");
-  });
-
-
 
 
   /* TABS HANDLING */
   $("#submenu1").click( function() {
     console.log('clic submenu1');
-    for(var i=0; i < 10; i++){
+    for(var i=0; i < 9; i++){
       $(arr[i]).hide();
     }
 
@@ -330,7 +233,7 @@ $(document).ready(function() {
 
   $("#submenu2").click( function() {
     console.log('clic submenu2');
-    for(var i=0; i < 10; i++){
+    for(var i=0; i < 9; i++){
       $(arr[i]).hide();
     }
 
@@ -364,7 +267,7 @@ $(document).ready(function() {
 
   $("#submenu3").click( function() {
     console.log('clic submenu3');
-    for(var i=0; i < 10; i++){
+    for(var i=0; i < 9; i++){
       $(arr[i]).hide();
     }
 
