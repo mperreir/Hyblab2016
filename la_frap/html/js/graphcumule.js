052d1964b9a3ceea95bc15f80e7117ab73b3d87e
Matthieu Perreira Da Silva <matthieu.perreiradasilva@univ-nantes.fr>
2016-03-10 11:13:59 +0100

    Few fixes for FRAP Project + France3 footer

--- la_frap/html/js/graphcumule.js
+++ la_frap/html/js/graphcumule.js
@@ -2,9 +2,11 @@ function cumule(id){
 	 if(id === "sub"){
             document.getElementById('finance').style.visibility = "visible";
             document.getElementById('rh').style.visibility = "hidden";
+						document.getElementById('graphCaption').textContent = "Evolution du volume financier des subventions en moyenne par radio";
             finance();}
      else if(id === "rh"){
             document.getElementById('finance').style.visibility = "hidden";
             document.getElementById('rh').style.visibility = "visible";
+						document.getElementById('graphCaption').textContent = "Evolution moyenne des effectifs par radio";
             rh();}
 }
